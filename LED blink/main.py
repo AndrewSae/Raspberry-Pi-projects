b7de20f26e1f463fae0c8779be6b6aef9271ef00
Andrew <84029016+AndrewSae@users.noreply.github.com>
2021-11-30 17:51:51 -0500

    Update main.py

--- LED blink/main.py
+++ LED blink/main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from gpiozero import LED # imports the required libraries
 from time import sleep
 
@@ -8,15 +7,3 @@ while True:    # a infinate loop
     led.on()    # turns on the led
     sleep(1)    # pauses the code for set time
     led.off()    # turns off the led
-=======
-from gpiozero import LED # imports the required libraries
-from time import sleep
-
-led = LED(17)    # sets the var "led" to the gpio pin 17 on the pi
-
-while True:    # a infinate loop
-    led.on()    # turns on the led
-    sleep(1)    # pauses the code for set time
-    led.off()    # turns off the led
->>>>>>> 9421207a437ae06476b66797ea4a58c6cc18aa07
-    sleep(1)
