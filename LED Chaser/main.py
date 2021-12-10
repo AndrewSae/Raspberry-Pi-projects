fd3cc8464751750f509439f19e2094db5195e151
Andrew <84029016+AndrewSae@users.noreply.github.com>
2021-12-10 07:56:03 -0500

    fixed code

--- LED Chaser/main.py
+++ LED Chaser/main.py
@@ -6,16 +6,16 @@ led = [
     LED(27),
     LED(22),
     LED(10),
-    led(9),
-    led(11),
+    LED(9),
+    LED(11),
     LED(5)
     ]    # makes a array called "led" that stores the led GPIO pin numbers
 
 x = 0    # makes a var "x" and sets it to 0
 
 while True:    # a infinite loop
-    while x <= len(led):    # if "x" is less or equal to the length of the array "led" the indented code will rin
-        led[x].on()    # turns on the led that is at the posios of "x" in the array
+    while x <= len(led):    # if "x" is less or equal to the length of the array "led" the indented code will run
+        led[x].on()    # turns on the led that is at the position of "x" in the array
         sleep(.5)    # pauses the code for ".5" seconds
         led[x].off()    # turns off the led that is at the position of "x" in the array
         x += 1    # adds 1 to "x"
