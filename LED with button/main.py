847c578283f60ad963bbe42e7b3cccc6a133d1f1
Andrew <84029016+AndrewSae@users.noreply.github.com>
2021-12-09 19:14:36 -0500

    Update main.py

--- LED with button/main.py
+++ LED with button/main.py
@@ -5,7 +5,7 @@ from time import sleep
 led = LED(17)    # sets the var "led" to the gpio pin 17 on the pi
 button = Button(27)    # sets the var "button" to the gpio pin 27 on the pi
 
-while True:    # a infinate loop
+while True:    # a infinite loop
     if button.wait_for_press():    # if the button is pressed the indented code will run
         led.on()    # turns the led on
     led.off()    # turns off the led once the button is not being pressed
